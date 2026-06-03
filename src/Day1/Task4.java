b472370dea6e8716876d85eafeec3eb258df3990
Manish Kumar <itsmenishan.12@gmail.com>
2026-06-03 18:08:01 +0530

    task 4

---
+++ src/Day1/Task4.java
@@ -0,0 +1,18 @@
+package Day1;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        double salary = 75000.99;
+        int salary2 = (int) salary;
+
+        System.out.println("Double Salary " + salary);
+        System.out.println("Integer Salary  : " + salary2);
+
+        float firstVal = 4.8f;
+        int floatToInt = (int) firstVal;
+
+        System.out.println("Float val : " + firstVal);
+        System.out.println("Float to Int : " + floatToInt);
+    }
+}
