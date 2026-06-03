79a8a9e12008fc9d5e1b5330b2b6c8a6fd40a9be
Manish Kumar <itsmenishan.12@gmail.com>
2026-06-03 18:04:08 +0530

    Task 3

---
+++ src/Day1/Task3.java
@@ -0,0 +1,17 @@
+package Day1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        byte firstVal = 10;
+
+        short shortValue = firstVal;
+        int intValue = shortValue;
+        long longValue = intValue;
+        float floatValue = longValue;
+        double doubleValue = floatValue;
+
+        System.out.println("Printing values : byte : "+firstVal +" -> short "
+                + shortValue + " -> int " + intValue + " -> long " + longValue + " -> float " + floatValue + " -> double " + doubleValue + " -> End");
+
+    }
+}
