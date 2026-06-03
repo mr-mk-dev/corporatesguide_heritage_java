4eaa2d086f0285a51d373ff4d7d5b82cc8035118
Manish Kumar <itsmenishan.12@gmail.com>
2026-06-03 18:14:26 +0530

    task 6

---
+++ src/Day1/Task6.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        String val = "  Manish Kumar";
+        System.out.println("After trim : " + val.trim());
+        System.out.println("After upper case : " + val.toUpperCase());
+        System.out.println("After Lower case : " + val.toLowerCase());
+        System.out.println("Length : " + val.length());
+    }
+}
